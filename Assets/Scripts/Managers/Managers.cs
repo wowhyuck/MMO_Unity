7f46b51e842706639f83538cccb2d293cd796493
Joohyuck <joohyuck.seo@gmail.com>
2021-03-14 15:17:24 +0900

    Singleton 패턴

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -4,18 +4,34 @@ using UnityEngine;
 
 public class Managers : MonoBehaviour
 {
+    static Managers s_instance;       // 유일성이 보장된다
+    public static Managers Instance { get { Init(); return s_instance; } }      // 유일한 매니저를 갖고온다
+
     // Start is called before the first frame update
     void Start()
     {
-        // 초기화
-
-
-        
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    static void Init()
+    {
+        if(s_instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            s_instance = go.GetComponent<Managers>();
+        }
     }
 }

7081e9474074d21c83e286a5799571a348b9c66f
Joohyuck <joohyuck.seo@gmail.com>
2021-03-14 14:34:09 +0900

    Component 패턴

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloUnity : MonoBehaviour
+public class Test : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello Unity");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(new Vector3(1.0f, 1.0f, 1.0f));
     }
 }

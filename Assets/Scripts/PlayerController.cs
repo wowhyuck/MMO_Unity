2fbc7a364e0d70871243df22f8cd9773ec25421c
Joohyuck <joohyuck.seo@gmail.com>
2021-03-16 03:21:26 +0900

    플레이어 설정 및 유니티 모델 추가

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    // GameObject (Player)
+        // Transform
+        // PlayerController (*)
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+        if (Input.GetKey(KeyCode.S))
+            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+        if (Input.GetKey(KeyCode.A))
+            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+        if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+    }
+}

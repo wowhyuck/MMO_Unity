b2c1b2fd9aa7dbf200791d8f2eedf56f9695f156
Joohyuck <joohyuck.seo@gmail.com>
2021-03-16 13:09:35 +0900

    Input Manager

--- Assets/Scripts/Managers/Managers.cs
+++ Assets/Scripts/Managers/Managers.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class Managers : MonoBehaviour
 {
     static Managers s_instance;       // 유일성이 보장된다
-    public static Managers Instance { get { Init(); return s_instance; } }      // 유일한 매니저를 갖고온다
+    static Managers Instance { get { Init(); return s_instance; } }      // 유일한 매니저를 갖고온다
+    
+    InputManager _input = new InputManager();
+    public static InputManager Input { get { return Instance._input; } }
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +19,7 @@ public class Managers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _input.OnUpdate();
     }
 
     static void Init()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,22 +9,17 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
+    }
+
+    void Update()
+    {
 
     }
 
-    float _yAngle = 0.0f;
-    void Update()
+    void OnKeyboard()
     {
-        _yAngle += Time.deltaTime * 100;
-
-        // 절대 회전값
-        //transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f);
-
-        // +- delta
-        //transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100.0f, 0.0f));
-
-        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
-
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.forward * Time.deltaTime * _speed;

cc0b0425bb9b7628dd7ddbc71099a5fe262bee40
Joohyuck <joohyuck.seo@gmail.com>
2021-03-16 09:30:34 +0900

    Position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    float _speed = 10.0f;
+
     void Start()
     {
         
@@ -14,13 +17,20 @@ public class PlayerController : MonoBehaviour
         // PlayerController (*)
     void Update()
     {
+        // Local -> World
+        // TransformDirection
+
+        // World -> Local
+        // InverseTransformDirection
+
         if (Input.GetKey(KeyCode.W))
-            transform.position += new Vector3(0.0f, 0.0f, 1.0f);
+            //transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.S))
-            transform.position -= new Vector3(0.0f, 0.0f, 1.0f);
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.A))
-            transform.position -= new Vector3(1.0f, 0.0f, 0.0f);
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
         if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(1.0f, 0.0f, 0.0f);
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
     }
 }

8c58ab8b40a62ecf6fbdf24c936f4c0615d9922d
Joohyuck <joohyuck.seo@gmail.com>
2021-05-04 05:15:33 +0900

    Camera

---
+++ Assets/Scripts/Utils/Define.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum MouseEvent
+    {
+        Press,
+        Click,
+    }
+
+    public enum CameraMode
+    {
+        QuarterView,
+    }
+
+
+
+}

93dedc14ad63a79c1f54c8fceef7c99c04e50ea9
Joohyuck <joohyuck.seo@gmail.com>
2021-05-10 02:50:14 +0900

    State Machine

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
     float _speed = 10.0f;
 
     Vector3 _destPos;
-    float wait_run_ratio = 0;
 
     public enum PlayerState
     {
@@ -65,18 +64,15 @@ public class PlayerController : MonoBehaviour
 
         // 애니메이션
         Animator anim = GetComponent<Animator>();
-        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 1, 10.0f * Time.deltaTime);
-        anim.SetFloat("wait_run_ratio", wait_run_ratio);
-        anim.Play("WAIT_RUN");
+        // 현재 게임 상태에 대한 정보를 넘겨준다.
+        anim.SetFloat("speed", _speed);
     }
 
     void UpdateIdle()
     {
         // 애니메이션
         Animator anim = GetComponent<Animator>();
-        wait_run_ratio = Mathf.Lerp(wait_run_ratio, 0, 10.0f * Time.deltaTime);
-        anim.SetFloat("wait_run_ratio", wait_run_ratio);
-        anim.Play("WAIT_RUN");
+        anim.SetFloat("speed", 0);
     }
 
     void OnMouseClicked(Define.MouseEvent evt)

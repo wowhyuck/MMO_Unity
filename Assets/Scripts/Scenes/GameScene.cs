24cc45feecd5fb7676467287d256d86f81f6e44b
Joohyuck <joohyuck.seo@gmail.com>
2021-05-26 03:16:08 +0900

    Coroutine

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class GameScene : BaseScene
 {
+    Coroutine co;
+
     protected override void Init()
     {
         base.Init();
 
         SceneType = Define.Scene.Game;
 
-
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        for (int i = 0; i < 5; i++)
-            Managers.Resource.Instantiate("UnityChan");
+        co = StartCoroutine("ExplodeAfterSeconds", 4.0f);
+        StartCoroutine("CoStopExplode", 2.0f);
+    }
+
+    IEnumerator CoStopExplode(float seconds)
+    {
+        Debug.Log("Stop Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Stop Excute!");
+        if (co != null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
+    }
+
+    IEnumerator ExplodeAfterSeconds(float seconds)
+    {
+        Debug.Log("Explode Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Explode Excute!");
+        co = null;
     }
 
     public override void Clear()
     {
-        throw new System.NotImplementedException();
+        
     }
 }
